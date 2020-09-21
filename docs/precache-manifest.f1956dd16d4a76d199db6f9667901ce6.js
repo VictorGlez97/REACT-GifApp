47a29f8d89561ad2121fedf9715b5ab14d85d6b5
Victor Gonzalez Arreola <vichgleza@gmail.com>
2020-09-21 16:32:57 -0500

    docs agregado

---
+++ docs/precache-manifest.f1956dd16d4a76d199db6f9667901ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731bc4741cd4f08d72548037a22b1fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9daca07178ab717bafa",
+    "url": "/static/css/main.1cd7f5fc.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9daca07178ab717bafa",
+    "url": "/static/js/main.5bef35e6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
